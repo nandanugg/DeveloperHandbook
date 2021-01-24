b8966f269ba7d3179e9256bfc2cfdc947856d7a1
nandanugg <navy.techno@gmail.com>
2021-01-25 05:21:50 +0700

    Add function explanation

--- getting-started/functions/functions.js
+++ getting-started/functions/functions.js
@@ -8,14 +8,14 @@
 
 
 // How to create a function
-//       👇 function name 👇 parameter
 function addDollarSign(number) {
-  const output = `$${number}` // 👈 process
-  return output // 👈 return / result of the function
+  const output = `$${number}`
+  return output
 }
 // How to use a function
 // if we don't need the return
-addDollarSign(200)
+addDollarSign(200) // 👈 remember to put the "()" after function name, otherwise the function wouldn't run / execute
+
 // if we need the return to be assigned with a value
 let itemValue = addDollarSign(200)
 console.log('from addDollarSign()', itemValue) // $200
@@ -23,40 +23,25 @@ console.log('from addDollarSign()', itemValue) // $200
 function getHello() {
   return 'hello'
 }
-// we need the return to be logged into terminal
+// we can put the function return value directly to another function
 console.log('from getHello():', getHello()); // hello
 
+// A function also able to not have a return value
 function sayHello() {
   console.log('from sayHello():', "hello");
 }
 
-// Function concepts
+/**
+ * If a function doesn't have a return statement / value
+ * or we just don't need the return value, we
+ * can just type like this
+ */
+sayHello()
 
 /**
- * Impure function
- * 
- * Is a function that uses any functions or variables outside
- * of its function
+ * If a function doesn't have a return statement / value
+ * and we still want to try get the return, it will
+ * be retuned with "undefined"
  */
-let startingMessage = 'The message is :'
-
-function showMessage(message) {
-  const output = `${startingMessage} ${message}`
-  console.log('from showMessage():', output)
-}
-showMessage("Halo semua!")
-
-/**
- * Pure function
- *
- * Is a function that only uses functions or variables inside
- * of its function
- */
-function addExclamationMark(word) {
-  const output = `${word}!`
-  return output
-}
-
-var addedExclamationWord = addExclamationMark("Budi")
-console.log('from addExclamationMark():', addedExclamationWord)
-
+let functionValue = sayHello()
+console.log("functionValue:", functionValue); // undefined

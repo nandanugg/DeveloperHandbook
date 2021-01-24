9fdd141e8582cd90a5ba4179823fbac11529da17
nandanugg <navy.techno@gmail.com>
2021-01-25 04:11:57 +0700

    Add terniary and logical operator explanation

--- getting-started/data-types/booleanPower.js
+++ getting-started/data-types/booleanPower.js
@@ -8,22 +8,37 @@
 let isRaining = false
 let isSunny = true
 
-// How to use it in if
+// How to use it in if statement
 if (isRaining) {
-  console.log('IsRaining if:', "Today is raining"); // this will be called
+  console.log('IsRaining if:', "Today is raining"); // "Today is raining"
 } else {
   console.log('IsRaining if:', "Today isn't raining");
 }
 
-// Boolean can also be written in boolean operator
-// Below opearor (==) is called "equal to" comparison operator
+// We can shorten the "if" statement by using "Conditional terniary operator"
+let isHavePhone = true
+let isListenMusic = isHavePhone ? "Let's hear music!" : "Nah, let's hear it later"
+console.log("isListenMusic:", isListenMusic); // "Let's hear music!"
+
+/**
+ * Boolean can also be written in boolean comparison operator
+ * means the return of this operator will be boolean
+ * 
+ * Below opearor (==) is called "equal to" comparison operator
+ * Comparison operator will always return a boolean
+ */
 let isSame = 1 == 1
 console.log('IsSame:', isSame); // true
 
-// Below operator (&&) is called "and" logical operator
-// the (!) is called negation, it reverts the value from false to true and vice versa
-let isGoodDay = !isRaining && isSunny // if isRaining = false AND isSunny = true
-console.log('isGoodDay:', isGoodDay); // true
+/**
+ * Logical operator it's a way to compare two booleans
+ * 
+ * Below operator (&&) is called "and" logical operator
+ */
+let isHaveDebt = true
+let isHaveMoney = true
+let isPaydebt = isHaveDebt && isHaveMoney
+console.log('isPaydebt:', isPaydebt); // true
 
 // More about boolean operators in https://www.w3schools.com/js/js_comparisons.asp
 
@@ -40,37 +55,78 @@ console.log('Boolean plus number:', boolean + number) // 2
  * 3. zero and negative numbers (0)
  * 4. null
  * 5. undefined
+ * 
+ * They are called "falsy" values
  */
 
 // empty string falsy
 let emptyString = ''
 if (emptyString) {
-  console.log("emptyString:", "A");
+  console.log("emptyString:", "If we fill the string, it will be true");
 } else {
-  console.log("emptyString:", "B"); // this will be shown
+  console.log("emptyString:", "Empty string is false"); // "Empty string is false"
 }
 
 // zero falsy
 let zero = 0
 if (zero) {
-  console.log("zero:", "A");
+  console.log("zero:", "If we increase the number, it will be true");
 } else {
-  console.log("zero:", "B"); // this will be shown
+  console.log("zero:", "If we decrease the number to zero and minus values, it will be false");
 }
 
 // null falsy
 let nullVariable = null
 if (nullVariable) {
-  console.log("nullVariable:", "A");
+  console.log("nullVariable:", "If we change the null to another non falsy value, it will be true");
 } else {
-  console.log("nullVariable:", "B"); // this will be shown
+  console.log("nullVariable:", "If we leave the value to be null, it will be false");
 }
 
 // undefined falsy
 let undefinedVariable;
 if (undefinedVariable) {
-  console.log("undefinedVariable:", "A");
+  console.log("undefinedVariable:", "If we fill the variable with a non falsy value, it will be true");
 } else {
-  console.log("undefinedVariable:", "B"); // this will be shown
+  console.log("undefinedVariable:", "If we leave the variable, not fill it with anything, it will be false");
 }
 // More about falsy values in https://developer.mozilla.org/en-US/docs/Glossary/Falsy
+
+/**
+ * Logical operators and falsy values
+ * 
+ * In Javascript, we often use logical / terniary opertor to decide what value
+ * should be inside a string
+ */
+
+// Terniary operator with comparison operator
+let userAge = 50
+
+let ageMessage = userAge > 0 ? "Thank you!" : "Please input the right age number"
+console.log("ageMessage", ageMessage); // "Thank you!"
+
+// Terniary operator with falsy operator
+
+let userName = ""
+let nameMessage = userName ? "Thank you!" : "Please input your name"
+console.log("nameMessage", nameMessage); // "Thank you!"
+
+// Logical (or) operator with truthy
+let userParentAge = 55
+let parentAgeMessage = userParentAge || "Please input the right age number"
+console.log("parentAgeMessage", parentAgeMessage); // 55
+
+// Logical (or) operator with falsy
+let userPetAge = 0
+let petNameMessage = userPetAge || "Please input the right age number"
+console.log("petNameMessage", petNameMessage); // "Please input the right age number"
+
+// Logical (and) operator with truthy
+let userParentName = ""
+let parentNameMessage = userParentName && "Thank you for inputting username"
+console.log("petNameMessage", petNameMessage); // "Thank you for inputting username"
+
+// Logical (and) operator with falsy
+let userSiblingName = ""
+let siblingNameMessage = userSiblingName && "Thank you for inputting username"
+console.log("siblingNameMessage", siblingNameMessage); // ""
